f5886b6412b7bfe920f78771863ef93010f4c9f2
volhazhukouskaya1 <volha_zhukouskaya@outlook.com>
2024-05-16 19:52:03 +0300

    task 4-1(2)

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,10 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
-        if (5 < 3)
-        {
-            System.out.println("true");
-        }
+       boolean r = 5<3;
+        if (false)
+        { System.out.println("true");}
         else
         {
             System.out.println("false");
